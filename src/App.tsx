1f92ba4bcc274ec902b97e0c839cac101a4c51a7
Rachmat Rizki <rahmatrizki089@gmail.com>
2025-09-12 18:10:24 +0700

    add: custom style label

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,9 @@ import Label from "./components/Label";
 
 function App() {
   return (
-    <div className="h-screen bg-black flex justify-center items-center">
-      <Label text="Hello World" />
+    <div className="h-screen bg-black flex justify-center items-center flex-col">
+      <Label text="Hello World" size="lg" />
+      <Label text="This is Gray" size="sm" color="gray" />
     </div>
   );
 }
--- src/components/Label.tsx
+++ src/components/Label.tsx
@@ -1,7 +1,30 @@
 type LabelProps = {
   text: string;
+  size?: "sm" | "md" | "lg";
+  color?: "white" | "gray";
 };
 
-export default function Label({ text }: LabelProps) {
-  return <label className="text-white text-sm font-medium">{text}</label>;
+export default function Label({
+  text,
+  size = "md",
+  color = "white",
+}: LabelProps) {
+  const sizeClasses = {
+    sm: "text-sm",
+    md: "text-md",
+    lg: "text-lg",
+  };
+
+  const colorClasses = {
+    white: "text-white",
+    gray: "text-gray-100",
+  };
+
+  return (
+    <label
+      className={`${sizeClasses[size]} ${colorClasses[color]} font-medium`}
+    >
+      {text}
+    </label>
+  );
 }
