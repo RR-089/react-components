ff0b13e6979496a0d263b321c812ab797afd9ff5
Rachmat Rizki <rahmatrizki089@gmail.com>
2025-09-14 21:03:01 +0700

    add: more props at card

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,13 +1,17 @@
-import type React from "react";
+import Button from "./Button";
 
 type CardProps = {
-  children: React.ReactNode;
+  title: string;
+  description: string;
+  onClick?: () => void;
 };
 
-export default function Card({ children }: CardProps) {
+export default function Card({ title, description, onClick }: CardProps) {
   return (
     <div className="bg-white shadow-md rounded-2xl p-4 border border-gray-200">
-      {children}
+      <h2 className="text-lg font-bold mb-2">{title}</h2>
+      <p className="text-sm mb-4 text-gray-600">{description}</p>
+      {onClick && <Button onClick={onClick}>Check More</Button>}
     </div>
   );
 }
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,26 +1,23 @@
-import Button from "../components/Button";
 import Card from "../components/Card";
 
 export default function Home() {
   return (
     <main className="grid grid-cols-1 md:grid-cols-3 gap-4 p-4">
-      <Card>
-        <h2 className="text-lg font-bold mb-2">Card 1</h2>
-        <p className="text-sm text-gray-600 mb-4">This is simple card.</p>
-        <Button>Action</Button>
-      </Card>
-
-      <Card>
-        <h2 className="text-lg font-bold mb-2">Card 2</h2>
-        <p className="text-sm text-gray-600 mb-4">Cards are reuseable!</p>
-        <Button variant="secondary">Secondary</Button>
-      </Card>
-
-      <Card>
-        <h2 className="text-lg font-bold mb-2">Card 3</h2>
-        <p className="text-sm text-gray-600 mb-4">Responsive grid layout</p>
-        <Button variant="outline">Outline</Button>
-      </Card>
+      <Card
+        title="Card 1"
+        description="This is simple card."
+        onClick={() => alert("Card 1")}
+      />
+      <Card
+        title="Card 2"
+        description="Cards are reuseable!"
+        onClick={() => alert("Card 2")}
+      />
+      <Card
+        title="Card 3"
+        description="Responsive grid layout"
+        onClick={() => alert("Card 3")}
+      />
     </main>
   );
 }
